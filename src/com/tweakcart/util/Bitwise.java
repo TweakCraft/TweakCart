930defb0f58bfd41e3cf904b881adcb3de1735f3
Koen Scheres <koen_75_@hotmail.com>
2011-07-25 20:03:57 +0200

    No stupid byteMap, using intMap Fix'd some stuff, added debug lines (thanks @windwarrior)

--- src/com/tweakcart/util/Bitwise.java
+++ src/com/tweakcart/util/Bitwise.java
@@ -6,24 +6,20 @@ package com.tweakcart.util;
  * Date: 28-6-11
  * Time: 13:44
  */
-public final class Bitwise
-{
+public final class Bitwise {
     public static short MaxBits = 8;
 
-    public static short packBits(short a, short b, short bitsWidth)
-    {
+    public static short packBits(short a, short b, short bitsWidth) {
         bitsWidth = (short) (Bitwise.MaxBits - bitsWidth);
         return (short) ((a << bitsWidth) | b);
     }
 
-    public static short getHighBits(short a, short bitsWidth)
-    {
+    public static short getHighBits(short a, short bitsWidth) {
         bitsWidth = (short) (Bitwise.MaxBits - bitsWidth);
         return (short) (a >> bitsWidth);
     }
 
-    public static short getLowBits(short a, short Highbit)
-    {
+    public static short getLowBits(short a, short Highbit) {
         //2 pow 6 min 1;
         Highbit = (short) (Math.pow(2, (Bitwise.MaxBits - Highbit)) - 1);
         return (short) (a & Highbit);
