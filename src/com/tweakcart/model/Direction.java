d1382dee4bfdbbe595877684f12cd9b437ce7c70
Koen Scheres <koen_75_@hotmail.com>
2011-07-31 18:41:34 +0200

    Hill bug

--- src/com/tweakcart/model/Direction.java
+++ src/com/tweakcart/model/Direction.java
@@ -1,5 +1,6 @@
 package com.tweakcart.model;
 
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.util.Vector;
 
@@ -149,8 +150,9 @@ public enum Direction {
         return getDirection(modx, 0, modz);
     }
 
-    public static final Direction getVerticalDirection(Vector velocity) {
-        int mody = (velocity.getY() == 0 ? 0 : velocity.getY() > 0 ? 1 : -1);
+    public static final Direction getVerticalDirection(Location cartLocation) {
+        int mody = (cartLocation.getPitch() == 0 ? 0 : cartLocation.getPitch() > 0 ? 1 : -1);
+        System.out.println("Going: " + directions[1][mody + 1][1]);
         return getDirection(0, mody, 0);
     }
 }
